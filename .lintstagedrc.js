5d2f193267244075538a5fcf2d58193f9b2ddb1b
Brian Fajardo <brian.fajardo@osedea.com>
2019-03-15 00:22:27 -0400

    Replaced Web Font Loader with font-display. Changed format script. Formatting

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,5 @@
 module.exports = {
-    '*.scss': 'stylelint "scss/*.scss" --config "./.stylelintrc.json" --syntax=scss --fix',
+    '*.scss':
+        'stylelint "scss/*.scss" --config "./.stylelintrc.json" --syntax=scss --fix',
     '*.{html,scss,md,json}': ['prettier --write', 'git add'],
 }
